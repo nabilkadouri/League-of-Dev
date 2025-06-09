b4af70dad739c3e7ad79133f73e2047be9e98b1d
Nabil Kadouri <kadourinabil7@gmail.com>
2025-06-09 19:03:44 +0200

    refactor: Initialise et lance le jeu via Main
    La classe `Main` a été mise à jour pour servir de point d'entrée principal au jeu. Elle initialise le `CombatManager` et démarre la partie, consolidant le lancement du jeu dans une structure claire.

---
+++ src/rpg/Main.java
@@ -0,0 +1,11 @@
+package rpg;
+
+import rpg.utils.CombatManager;
+
+
+public class Main {
+    public static void main(String[] args) {
+        CombatManager combat = new CombatManager();
+        combat.demarrerPartie();
+    }
+}
