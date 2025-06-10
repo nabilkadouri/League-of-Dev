c4918b766b17469e5681ed43454d0b0297376e8f
Nabil Kadouri <kadourinabil7@gmail.com>
2025-06-10 22:26:19 +0200

    feat: - Supression de l'attribut mana et getter et setter. - Modifications des méthodes utiliserPouvoir() et getDegatsInfliges()

--- src/rpg/personnages/Hero.java
+++ src/rpg/personnages/Hero.java
@@ -5,25 +5,18 @@ import rpg.interfaces.PouvoirSpecial;
 import java.util.Random;
 
 public class Hero extends Personnage implements PouvoirSpecial {
-    private int mana;
-    private int nombreDePotions = 1;
-    private int quantitePvPotion = 75;
-    private int compteurVictoire = 0;
+    protected int nombreDePotions;
+    protected int quantitePvPotion;
+    protected int compteurVictoire;
 
-    public Hero(String nom) {
-        super(nom, 150, 0, 10);
-        this.mana = 30;
+    public Hero(String nom, int pv, int attaque, int defense) {
+        super(nom,pv, attaque, defense);
         this.nombreDePotions = 1;
+        this.quantitePvPotion = 75;
+        this.compteurVictoire = 0;
     }
 
-    public int getMana() {
-        return mana;
-    }
-
-    public void setMana(int mana) {
-        this.mana = mana;
-    }
-
+    // --- Getters et Setters ---
     public int getNombreDePotions() {
         return nombreDePotions;
     }
@@ -32,69 +25,65 @@ public class Hero extends Personnage implements PouvoirSpecial {
         return quantitePvPotion;
     }
 
+
     public int getCompteurVictoire() {
         return compteurVictoire;
     }
 
-    @Override
-    public void utiliserPouvoir(Personnage cible) {
-        int coutMana = 10;
-        if(this.mana >= coutMana) {
-            int degatsSpeciaux = (getDegatsInfliges() * 2) - cible.getDefense();
-            cible.prendreDegat(degatsSpeciaux);
-            this.mana -= coutMana;
-            System.out.println("Attaque spéciale déclenchée sur " + cible.getNom() + " qui subit " + degatsSpeciaux + " de dégâts.");
-        }else {
-            System.out.println(this.nom +" n'a pas assez de mana pour l'attaque spéciale !");
-        }
-    }
+    // --- Méthodes utilisées pour tous les héros ---
 
+    /**
+     * Méthode pour utiliser une potion de soin.
+     * Logique commune à tous les héros.
+     */
     public void utiliserPotion() {
-        // Condition 1: Le héros a-t-il une potion ?
         if (nombreDePotions > 0) {
-            // Condition 2: Les PV actuels sont-ils entre 5 et 170 (inclus) ?
-            // On utilise également getPVMax() pour gérer le cas où la restauration dépasserait le max.
-            if (this.pv >= 5 && this.pv <= 170) {
-                // Restaure les PV, mais ne dépasse pas les PV max
+            if (this.pv >= 1 && this.pv <= 120) {
                 int pvAvantPotion = this.pv;
                 this.pv = Math.min(this.pv + quantitePvPotion, this.pvMax);
-                nombreDePotions--; // Consomme la potion
+                nombreDePotions--;
                 System.out.println("Vous avez utilisé votre potion ! Vous avez restauré " + (this.pv - pvAvantPotion) + " PV.");
             } else {
-                System.out.println("Vous ne pouvez pas utiliser la potion maintenant. Vos PV sont trop bas (<5) ou trop élevés (>=175).");
+                System.out.println("Vous ne pouvez pas utiliser la potion maintenant. Vos PV sont trop élevés " + "("+ this.pv + "PV.)");
             }
         } else {
             System.out.println("Plus de potion dans ta besace !");
         }
     }
 
-    public void restaureMana(){
-        if(mana == 30) {
-            System.out.println("Réserve de mana pleine, bonus de victoire perdu !");
-        }else{
-            this.setMana(this.mana + 10);
-            System.out.println("Bonus de victoire : 10 de mana restauré.");
-        }
-    }
-
-    public int compteurVictoire(){
+    /**
+     * Incrémente le compteur de victoires du héros.
+     * Logique commune à tous les héros.
+     */
+    public int incrementerCompteurVictoire(){
         return compteurVictoire++;
     }
 
     /**
-     * Surcharge de la methode pour générer la valeur d'attaque du Héros.
-     * L'attaque sera 15,20,25,30 ou 35.
-     * @return La valeur d'attaque aléatoire (15,20,25,30 ou 35).
+     * Cette méthode définit l'attaque de base d'un héros.
+     * Elle est surchargée ici pour donner une logique générique à tous les héros
+     * avant d'être potentiellement surchargée à nouveau dans les sous-classes.
+     * @return Les dégâts "bruts" infligés par l'attaque de base du héros.
      */
     @Override
     protected int getDegatsInfliges() {
         Random random = new Random();
-        int choixMultiplicateur = random.nextInt(6);
-        return (choixMultiplicateur * 5) + 15;
+        return this.attaque + random.nextInt(5) - 2;
+    }
+
+    @Override
+    public void utiliserPouvoir(Personnage cible) {
+        int degatsBruts = this.getDegatsInfliges() +10;
+        int degatsInfliges = degatsBruts - cible.getDefense();
+        if(degatsInfliges <10){
+            degatsInfliges = 10;
+        }
+        cible.prendreDegat(degatsInfliges);
+        System.out.println(this.nom + "utilise attaque spéciale sur " + cible.getNom() + " et inflige " + degatsInfliges + " de dégâts !");
     }
 
     @Override
     public String toString() {
-        return super.toString() + "mana=" + mana;
+        return super.toString() +  "Potions=" + nombreDePotions + ", Victoires=" + compteurVictoire;
     }
 }
